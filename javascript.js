b6d0b06a63373b8d9adb3ee69216c937c864d09f
Lucas Long <lucaslong96@gmail.com>
2021-03-13 12:18:29 -0500

    Added variables for keeping track of values, and made isOperator().

--- javascript.js
+++ javascript.js
@@ -1,5 +1,11 @@
+const workingAreaDisplay = document.querySelector("#workingAreaDisplay");
 const inputDisplay = document.querySelector("#inputDisplay");
 const inputButtons = document.querySelectorAll(".inputButton");
+
+let firstValue = "";
+let secondValue = "";
+let operator = "";
+const operators = "+-*/";
 
 // apply click listeners for all of the input buttons
 inputButtons.forEach(button => {
@@ -9,6 +15,18 @@ inputButtons.forEach(button => {
 // adds text from the button to what's already in the input display
 function updateInputDisplay(e) {
     inputDisplay.textContent += e.target.textContent;
+}
+
+// checks if the current string from a button is one of of the operator buttons
+function isOperator(e) {
+    const temp = e.target;
+    
+    if (operators.includes(temp)) {
+        operator = e.target;
+        return true;
+    } else {
+        return false;
+    }
 }
 
 function add(x, y) {
